33febdba42101fb2a4440cd9e6f2a400dff3a942
MilletQ <niceback@live.com>
2025-07-16 17:15:10 +0800

    增加鼠标移入移出效果

--- 03_Todolist案例/todolist/src/components/Item/Item.jsx
+++ 03_Todolist案例/todolist/src/components/Item/Item.jsx
@@ -2,10 +2,25 @@ import React, { Component } from "react";
 import "./index.css"; // Importing CSS for styling
 
 export default class Item extends Component {
+  state = {
+    isMouseIn: false, // State to track mouse hover
+  };
+  handleMouseEnter = () => {
+    this.setState({ isMouseIn: true }); // Set state to true when mouse enters
+  };
+  handleMouseLeave = () => {
+    this.setState({ isMouseIn: false }); // Set state to false when mouse leaves
+  };
   render() {
     const { todo } = this.props; // Destructure todo from props
     return (
-      <li>
+      <li
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+        style={{
+          backgroundColor: this.state.isMouseIn ? "#d3cfcfff" : "#ffffffff",
+        }}
+      >
         <label>
           <input type="checkbox" />
           <span>{todo.name}</span>
